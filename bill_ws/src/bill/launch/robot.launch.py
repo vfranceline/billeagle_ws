7643f032bf41d815b61b9a376c469f5a4e2ba606
Vitória Matos <vitorianascimentomatos@gmail.com>
2025-02-07 16:31:52 -0300

    add rqt dos testes de sexta

--- bill_ws/src/bill/launch/robot.launch.py
+++ bill_ws/src/bill/launch/robot.launch.py
@@ -94,7 +94,7 @@ def generate_launch_description():
         executable='static_transform_publisher',
         name='static_transform_publisher',
         output='screen',
-        arguments=['0.0', '0.0', '0.0', '0', '0', '0', 'base_link', 'odom'],
+        arguments=['0.0', '0.0', '0.0', '0', '0', '0', 'base_link', 'odometry'],
     )
 
     tf2_node = Node(package='tf2_ros',
