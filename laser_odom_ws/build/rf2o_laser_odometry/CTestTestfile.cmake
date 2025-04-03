6176d2f311f51271c5c91f4c9fe1e116da736c95
vic <vitorianascimentomatos@gmail.com>
2025-04-03 17:34:47 -0300

    versão do ultimo teste

---
+++ laser_odom_ws/build/rf2o_laser_odometry/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bill8/billeagle_ws/laser_odom_ws/src/rf2o_laser_odometry
+# Build directory: /home/bill8/billeagle_ws/laser_odom_ws/build/rf2o_laser_odometry
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
