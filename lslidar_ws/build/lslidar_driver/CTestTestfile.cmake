86eea41bb67e9ee9cf3a9145d5f1138aa2ccc72a
vic <vitorianascimentomatos@gmail.com>
2025-04-04 18:36:29 -0300

    add slam no amcl totalmente instavel

--- lslidar_ws/build/lslidar_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bill8/billeagle_ws/lslidar_ws/src/LSLIDAR_M10_N10_V2.5.2_230110_ROS2/lslidar_driver
-# Build directory: /home/bill8/billeagle_ws/lslidar_ws/build/lslidar_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
